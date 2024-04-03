00fb79e1a061359ae9e1631e23efa860db0391f7
ricardocoelhoLEI <202300255@estudantes.ips.pt>
2024-04-03 08:59:20 +0100

    Nível 1

---
+++ GroundTransportation.java
@@ -0,0 +1,17 @@
+
+/**
+ * Escreva uma descrição da classe GroundTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class GroundTransportation extends Transport
+{
+    private static final double HONORARY = 0.03;
+    
+    private String licensePlate;
+    
+    public GroundTransportation(){
+        
+    }
+}
